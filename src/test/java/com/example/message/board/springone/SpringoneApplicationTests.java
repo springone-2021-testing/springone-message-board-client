374d757af53a83921a27e60a683c6bc923603b68
Maria Gabriella Brodi <mbrodi@pivotal.io>
2021-08-14 15:27:51 -0400

    initial commit

---
+++ src/test/java/com/example/message/board/springone/SpringoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.message.board.springone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
