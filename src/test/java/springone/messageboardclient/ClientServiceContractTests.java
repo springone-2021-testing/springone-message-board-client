fef46ec5bdfb84021162f4b5934fe4fb5173750a
Cora Iberkleid <ciberkleid@gmail.com>
2021-08-26 09:47:58 -0400

    update code for contract with First_Last format for post

--- src/test/java/springone/messageboardclient/ClientServiceContractTests.java
+++ src/test/java/springone/messageboardclient/ClientServiceContractTests.java
@@ -11,7 +11,7 @@ import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties
 
 import java.util.List;
 
-@AutoConfigureStubRunner(ids = "springone:message-board-contracts",
+@AutoConfigureStubRunner(ids = "springone:message-board-contracts:1.0.0-B",
         stubsMode = StubRunnerProperties.StubsMode.LOCAL)
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.NONE)
 public class ClientServiceContractTests {
@@ -29,7 +29,7 @@ public class ClientServiceContractTests {
 
     @Test
     void shouldAddMessage() {
-        Result result = this.service.addMessage("Cora","Welcome to everyone!");
+        Result result = this.service.addMessage("Cora_Iberkleid","Welcome to everyone!");
         Assertions.assertEquals(result.getParameter(), "1");
         Assertions.assertEquals(result.getOperation(), "Create");
         Assertions.assertEquals(result.getStatus(), "Success");
@@ -39,7 +39,7 @@ public class ClientServiceContractTests {
     void shouldGetMessages() {
         List<Message> messageList = this.service.getMessages();
         Assertions.assertEquals(messageList.get(0).getId(), 1);
-        Assertions.assertEquals(messageList.get(0).getUsername(), "Cora");
+        Assertions.assertEquals(messageList.get(0).getUsername(), "Cora_Iberkleid");
         Assertions.assertEquals(messageList.get(0).getText(), "Welcome to everyone!");
     }
 
